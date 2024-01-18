00bcd159b16b4837c1026ffd9fefb988de811278
zhack47 <mesbah.zacharia@sfr.fr>
2024-01-18 13:52:08 +0100

    Factoring as an encoder-decoder model

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerMedNeXt.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerMedNeXt.py
@@ -244,17 +244,12 @@ class LayerNorm(nn.Module):
             return x
 
 
-class MedNeXt(nn.Module):
-
+class MedNeXtEncoder(nn.Module):
     def __init__(self,
                  in_channels: int,
                  n_channels: int,
-                 n_classes: int,
                  exp_r: int = 4,  # Expansion ratio as in Swin Transformers
                  kernel_size: int = 7,  # Ofcourse can test kernel_size
-                 enc_kernel_size: int = None,
-                 dec_kernel_size: int = None,
-                 deep_supervision: bool = False,  # Can be used to test deep supervision
                  do_res: bool = False,  # Can be used to individually test residual connection
                  do_res_up_down: bool = False,  # Additional 'res' connection on up and down convs
                  checkpoint_style: bool = None,  # Either inside block or outside block
@@ -264,10 +259,8 @@ class MedNeXt(nn.Module):
                  dim='3d',  # 2d or 3d
                  grn=False
                  ):
-
         super().__init__()
 
-        self.do_ds = deep_supervision
         assert checkpoint_style in [None, 'outside_block']
         self.inside_block_checkpointing = False
         self.outside_block_checkpointing = False
@@ -277,7 +270,6 @@ class MedNeXt(nn.Module):
 
         if kernel_size is not None:
             enc_kernel_size = kernel_size
-            dec_kernel_size = kernel_size
 
         if dim == '2d':
             conv = nn.Conv2d
@@ -386,6 +378,62 @@ class MedNeXt(nn.Module):
             dim=dim,
             grn=grn
         )
+
+    def forward(self, x):
+        ret = []
+        x = self.stem(x)
+        x = self.enc_block_0(x)
+        ret.append(x)
+
+        x = self.down_0(x)
+        x = self.enc_block_1(x)
+        ret.append(x)
+
+        x = self.down_1(x)
+        x = self.enc_block_2(x)
+        ret.append(x)
+
+        x = self.down_2(x)
+        x = self.enc_block_3(x)
+        ret.append(x)
+
+        x = self.down_3(x)
+        ret.append(x)
+        if self.return_skips:
+            return ret
+        else:
+            return ret[-1]
+
+class MedNeXtDecoder(nn.Module):
+    def __init__(self, in_channels: int,
+                 n_channels: int,
+                 n_classes: int,
+                 exp_r: int = 4,  # Expansion ratio as in Swin Transformers
+                 kernel_size: int = 7,  # Ofcourse can test kernel_size
+                 deep_supervision: bool = False,  # Can be used to test deep supervision
+                 do_res: bool = False,  # Can be used to individually test residual connection
+                 do_res_up_down: bool = False,  # Additional 'res' connection on up and down convs
+                 checkpoint_style: bool = None,  # Either inside block or outside block
+                 block_counts: list = [2, 2, 2, 2, 2, 2, 2, 2, 2],  # Can be used to test staging ratio:
+                 # [3,3,9,3] in Swin as opposed to [2,2,2,2,2] in nnUNet
+                 norm_type='group',
+                 dim='3d',  # 2d or 3d
+                 grn=False):
+        super().__init__()
+
+        if type(exp_r) == int:
+            exp_r = [exp_r for i in range(len(block_counts))]
+
+        self.do_ds = deep_supervision
+        assert checkpoint_style in [None, 'outside_block']
+        self.inside_block_checkpointing = False
+        self.outside_block_checkpointing = False
+        if checkpoint_style == 'outside_block':
+            self.outside_block_checkpointing = True
+        assert dim in ['2d', '3d']
+
+        if kernel_size is not None:
+            dec_kernel_size = kernel_size
 
         self.bottleneck = nn.Sequential(*[
             MedNeXtBlock(
@@ -513,6 +561,88 @@ class MedNeXt(nn.Module):
             self.out_4 = OutBlock(in_channels=n_channels * 16, n_classes=n_classes, dim=dim)
 
         self.block_counts = block_counts
+    def forward(self, x, skips):
+        x_res_3, x_res_2, x_res_1, x_res_0 = skips
+        x = self.bottleneck(x)
+        if self.do_ds:
+            x_ds_4 = self.out_4(x)
+
+        x_up_3 = self.up_3(x)
+        dec_x = x_res_3 + x_up_3
+        x = self.dec_block_3(dec_x)
+
+        if self.do_ds:
+            x_ds_3 = self.out_3(x)
+        del x_res_3, x_up_3
+
+        x_up_2 = self.up_2(x)
+        dec_x = x_res_2 + x_up_2
+        x = self.dec_block_2(dec_x)
+        if self.do_ds:
+            x_ds_2 = self.out_2(x)
+        del x_res_2, x_up_2
+
+        x_up_1 = self.up_1(x)
+        dec_x = x_res_1 + x_up_1
+        x = self.dec_block_1(dec_x)
+        if self.do_ds:
+            x_ds_1 = self.out_1(x)
+        del x_res_1, x_up_1
+
+        x_up_0 = self.up_0(x)
+        dec_x = x_res_0 + x_up_0
+        x = self.dec_block_0(dec_x)
+        del x_res_0, x_up_0, dec_x
+
+        x = self.out_0(x)
+
+        if self.do_ds:
+            return [x, x_ds_1, x_ds_2, x_ds_3, x_ds_4]
+        else:
+            return x
+class MedNeXt(nn.Module):
+
+    def __init__(self,
+                 in_channels: int,
+                 n_channels: int,
+                 n_classes: int,
+                 exp_r: int = 4,  # Expansion ratio as in Swin Transformers
+                 kernel_size: int = 7,  # Ofcourse can test kernel_size
+                 enc_kernel_size: int = None,
+                 dec_kernel_size: int = None,
+                 deep_supervision: bool = False,  # Can be used to test deep supervision
+                 do_res: bool = False,  # Can be used to individually test residual connection
+                 do_res_up_down: bool = False,  # Additional 'res' connection on up and down convs
+                 checkpoint_style: bool = None,  # Either inside block or outside block
+                 block_counts: list = [2, 2, 2, 2, 2, 2, 2, 2, 2],  # Can be used to test staging ratio:
+                 # [3,3,9,3] in Swin as opposed to [2,2,2,2,2] in nnUNet
+                 norm_type='group',
+                 dim='3d',  # 2d or 3d
+                 grn=False
+                 ):
+
+        super().__init__()
+
+        self.do_ds = deep_supervision
+        assert checkpoint_style in [None, 'outside_block']
+        self.inside_block_checkpointing = False
+        self.outside_block_checkpointing = False
+        if checkpoint_style == 'outside_block':
+            self.outside_block_checkpointing = True
+        assert dim in ['2d', '3d']
+
+        if kernel_size is not None:
+            enc_kernel_size = kernel_size
+            dec_kernel_size = kernel_size
+        self.encoder = MedNeXtEncoder(in_channels=in_channels, n_channels=n_channels, exp_r=exp_r,
+                                      kernel_size=enc_kernel_size, do_res=do_res, do_res_up_down=do_res_up_down,
+                                      checkpoint_style=checkpoint_style, block_counts=block_counts,
+                                      norm_type=norm_type, dim=dim, grn=grn)
+        self.decoder = MedNeXtDecoder(in_channels=in_channels, n_channels=n_channels, n_classes=n_classes,
+                                      exp_r=exp_r, kernel_size=dec_kernel_size,deep_supervision=deep_supervision,
+                                      do_res=do_res, do_res_up_down=do_res_up_down, checkpoint_style=checkpoint_style,
+                                      block_counts=block_counts, norm_type=norm_type, dim=dim, grn=grn)
+
 
     def iterative_checkpoint(self, sequential_block, x):
         """
@@ -527,96 +657,9 @@ class MedNeXt(nn.Module):
 
     def forward(self, x):
 
-        x = self.stem(x)
-        if self.outside_block_checkpointing:
-            x_res_0 = self.iterative_checkpoint(self.enc_block_0, x)
-            x = checkpoint.checkpoint(self.down_0, x_res_0, self.dummy_tensor)
-            x_res_1 = self.iterative_checkpoint(self.enc_block_1, x)
-            x = checkpoint.checkpoint(self.down_1, x_res_1, self.dummy_tensor)
-            x_res_2 = self.iterative_checkpoint(self.enc_block_2, x)
-            x = checkpoint.checkpoint(self.down_2, x_res_2, self.dummy_tensor)
-            x_res_3 = self.iterative_checkpoint(self.enc_block_3, x)
-            x = checkpoint.checkpoint(self.down_3, x_res_3, self.dummy_tensor)
-
-            x = self.iterative_checkpoint(self.bottleneck, x)
-            if self.do_ds:
-                x_ds_4 = checkpoint.checkpoint(self.out_4, x, self.dummy_tensor)
-
-            x_up_3 = checkpoint.checkpoint(self.up_3, x, self.dummy_tensor)
-            dec_x = x_res_3 + x_up_3
-            x = self.iterative_checkpoint(self.dec_block_3, dec_x)
-            if self.do_ds:
-                x_ds_3 = checkpoint.checkpoint(self.out_3, x, self.dummy_tensor)
-            del x_res_3, x_up_3
-
-            x_up_2 = checkpoint.checkpoint(self.up_2, x, self.dummy_tensor)
-            dec_x = x_res_2 + x_up_2
-            x = self.iterative_checkpoint(self.dec_block_2, dec_x)
-            if self.do_ds:
-                x_ds_2 = checkpoint.checkpoint(self.out_2, x, self.dummy_tensor)
-            del x_res_2, x_up_2
-
-            x_up_1 = checkpoint.checkpoint(self.up_1, x, self.dummy_tensor)
-            dec_x = x_res_1 + x_up_1
-            x = self.iterative_checkpoint(self.dec_block_1, dec_x)
-            if self.do_ds:
-                x_ds_1 = checkpoint.checkpoint(self.out_1, x, self.dummy_tensor)
-            del x_res_1, x_up_1
-
-            x_up_0 = checkpoint.checkpoint(self.up_0, x, self.dummy_tensor)
-            dec_x = x_res_0 + x_up_0
-            x = self.iterative_checkpoint(self.dec_block_0, dec_x)
-            del x_res_0, x_up_0, dec_x
-
-            x = checkpoint.checkpoint(self.out_0, x, self.dummy_tensor)
-
-        else:
-            x_res_0 = self.enc_block_0(x)
-            x = self.down_0(x_res_0)
-            x_res_1 = self.enc_block_1(x)
-            x = self.down_1(x_res_1)
-            x_res_2 = self.enc_block_2(x)
-            x = self.down_2(x_res_2)
-            x_res_3 = self.enc_block_3(x)
-            x = self.down_3(x_res_3)
-
-            x = self.bottleneck(x)
-            if self.do_ds:
-                x_ds_4 = self.out_4(x)
-
-            x_up_3 = self.up_3(x)
-            dec_x = x_res_3 + x_up_3
-            x = self.dec_block_3(dec_x)
-
-            if self.do_ds:
-                x_ds_3 = self.out_3(x)
-            del x_res_3, x_up_3
-
-            x_up_2 = self.up_2(x)
-            dec_x = x_res_2 + x_up_2
-            x = self.dec_block_2(dec_x)
-            if self.do_ds:
-                x_ds_2 = self.out_2(x)
-            del x_res_2, x_up_2
-
-            x_up_1 = self.up_1(x)
-            dec_x = x_res_1 + x_up_1
-            x = self.dec_block_1(dec_x)
-            if self.do_ds:
-                x_ds_1 = self.out_1(x)
-            del x_res_1, x_up_1
-
-            x_up_0 = self.up_0(x)
-            dec_x = x_res_0 + x_up_0
-            x = self.dec_block_0(dec_x)
-            del x_res_0, x_up_0, dec_x
-
-            x = self.out_0(x)
-
-        if self.do_ds:
-            return [x, x_ds_1, x_ds_2, x_ds_3, x_ds_4]
-        else:
-            return x
+        x, skips = self.encoder(x)
+        out = self.decoder(x, skips)
+        return out
 
 
 class nnUNetTrainer_Optim_and_LR(nnUNetTrainer):
@@ -635,28 +678,14 @@ class nnUNetTrainer_Optim_and_LR(nnUNetTrainer):
         num_of_outputs_in_mednext = 5
         self.configuration_manager.pool_op_kernel_sizes = [[2, 2, 2] for i in range(num_of_outputs_in_mednext + 1)]
 
-    def set_deep_supervision_enabled(self, enabled: bool):
-        """
-        This function is specific for the default architecture in nnU-Net. If you change the architecture, there are
-        chances you need to change this as well!
-        """
-        if self.is_ddp:
-            mod = self.network.module
-        else:
-            mod = self.network
-        if isinstance(mod, OptimizedModule):
-            mod = mod._orig_mod
-
-        mod.deep_supervision = enabled
-
-    #def configure_optimizers(self):
-    #    optimizer = torch.optim.AdamW(self.network.parameters(),
-    #                                       self.initial_lr,
-    #                                       weight_decay=self.weight_decay,
-    #                                       eps=1e-4  # 1e-8 might cause nans in fp16
-    #                                       )
-    #    lr_scheduler = PolyLRScheduler(optimizer, self.initial_lr, self.num_epochs)
-    #    return optimizer, lr_scheduler
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.network.parameters(),
+                                           self.initial_lr,
+                                           weight_decay=self.weight_decay,
+                                           eps=1e-4  # 1e-8 might cause nans in fp16
+                                           )
+        lr_scheduler = PolyLRScheduler(optimizer, self.initial_lr, self.num_epochs)
+        return optimizer, lr_scheduler
 
 
 class nnUNetTrainer_MedNeXt_S_kernel3(nnUNetTrainer_Optim_and_LR):
